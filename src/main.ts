b2817719e5f0913e69fefbe969f4af0ee567e9d2
duongLe <duong.le.cmc@bizzi.vn>
2022-07-21 17:09:53 +0700

    update pord

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   setupSwagger(app);
-  await app.listen(3000);
+  await app.listen(19021);
 }
 
 bootstrap();
